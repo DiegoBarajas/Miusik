d98c277249bf99ec9b73b5bdd3fb1f684afd6543
DiegoBarajas <desaubv@gmail.com>
2024-02-12 12:45:36 -0600

    Hoal

--- backend/src/classes/Sockets.js
+++ backend/src/classes/Sockets.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 class Sockets{
 
     constructor( Server ){
@@ -9,9 +12,27 @@ class Sockets{
     socketEvents(){
         this.io.on('connection', (socket) => {
             console.log('New Connection:', socket);
+
+            socket.on('getAllSongs', () => {
+                console.log(getAllFiles(path.resolve(__dirname, '../media')));
+            });
+
         });
     }
 
 }
 
 module.exports = Sockets;
+
+function getAllFiles(folder){
+    return fs.readdirSync(folder)
+        .map(nombreArchivo => {
+            const rutaArchivo = path.join(folder, nombreArchivo);
+            if (fs.statSync(rutaArchivo).isFile()) return folder + nombreArchivo;
+        })
+        .filter(nombreArchivo => nombreArchivo !== undefined);
+}
+
+function readFilesArray(array){
+    
+}
